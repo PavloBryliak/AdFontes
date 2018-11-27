d21e1ba8ad688894828db67efe0b22fa9a9f60f0
oleksandrpotapov <opotapov@ucu.edu.ua>
2018-11-27 18:46:45 +0200

    1st commit

--- data/warehouse.py
+++ data/warehouse.py
@@ -1,11 +1,16 @@
+storage_dict = dict()
 with open('warehouse.txt') as fp:
-    number_of_warehouses =
+    # number_of_warehouses =
     line = fp.readline()
-    cnt = 1
-    while line:
-        print("Line {}: {}".format(cnt, line.strip()))
+    for i in range(int(line)):
         line = fp.readline()
-        cnt += 1
+        cord = tuple(map(int, line.split()))
+        line = fp.readline()
+        quantity = list(map(int, line.split()))
+        storage_dict[cord] = quantity
+print(storage_dict)
+
+
 
 
     #warehouse = list(map(int, file_contents.split()))
